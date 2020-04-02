f8f78a6e003741937f91057fca4b20e356bb940a
Unmesh Chowdhury <unmeshchow@gmail.com>
2020-04-02 18:55:11 +0600

    Implement state management with context api

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+const Search = () => {
+  const { searchUsers, users, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
+
   const [text, setText] = useState('');
 
   const onSubmit = e => {
@@ -33,20 +37,13 @@ const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
           className="btn btn-dark btn-block"
         />
       </form>
-      {showClear && (
+      {users.length > 0 && (
         <button className="btn btn-light btn-block" onClick={clearUsers}>
           Clear
         </button>
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
